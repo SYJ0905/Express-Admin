cccb75393210be0c9e024947e76931acc0848a8c
Cloud <BetaPhoenixSNSD@gmail.com>
2020-01-10 17:40:46 +0800

    新增登入功能

--- routes/login.js
+++ routes/login.js
@@ -1,15 +1,29 @@
 const express = require('express');
 
 const router = express.Router();
+const firebase = require('../plugins/firebase');
+
+const firebaseAuth = firebase.auth();
 
 // route: /login
 router.get('/', (req, res) => {
   res.render('login', {
     title: '登入',
+    errorMsg: req.flash('errorMsg'),
   });
 });
 
-router.post('/', () => {
-
+router.post('/', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.passwd;
+  firebaseAuth.signInWithEmailAndPassword(email, password)
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch((error) => {
+      const errorMsg = error.message;
+      req.flash('errorMsg', errorMsg);
+      res.redirect('/login');
+    });
 });
 module.exports = router;
