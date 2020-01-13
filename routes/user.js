bed894ec8b785dd5aae0d82649739d68e4f1c94e
Cloud <BetaPhoenixSNSD@gmail.com>
2020-01-13 14:43:34 +0800

    新增會員專區 middleware

--- routes/user.js
+++ routes/user.js
@@ -2,14 +2,25 @@ const express = require('express');
 
 const router = express.Router();
 const firebaseAdmin = require('../plugins/firebase-admin');
+
+// middleware
+// check login
+const login = (req, res, next) => {
+  const uid = req.session.uid;
+  if (uid) {
+    next();
+  } else {
+    res.redirect('/');
+  }
+};
+
 // route: /user
-router.get('/', (req, res) => {
-  firebaseAdmin.ref(`user/${req.session.uid}`)
-    .once('value', (dataSnapshot) => {
-      res.render('user', {
-        title: '會員專區',
-        nickname: dataSnapshot.val().nickname,
-      });
+router.get('/', login, (req, res) => {
+  firebaseAdmin.ref(`user/${req.session.uid}`).once('value', (dataSnapshot) => {
+    res.render('user', {
+      title: '會員專區',
+      nickname: dataSnapshot.val().nickname,
     });
+  });
 });
 module.exports = router;
