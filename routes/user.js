3150062dc27a408f9f2a8a4bae58deeb0a4bc934
Cloud <BetaPhoenixSNSD@gmail.com>
2020-01-13 14:17:03 +0800

    新增會員專區判定

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,15 @@
 const express = require('express');
 
 const router = express.Router();
-
+const firebaseAdmin = require('../plugins/firebase-admin');
 // route: /user
 router.get('/', (req, res) => {
-  res.render('user', {
-    title: '會員專區',
-  });
+  firebaseAdmin.ref(`user/${req.session.uid}`)
+    .once('value', (dataSnapshot) => {
+      res.render('user', {
+        title: '會員專區',
+        nickname: dataSnapshot.val().nickname,
+      });
+    });
 });
 module.exports = router;
